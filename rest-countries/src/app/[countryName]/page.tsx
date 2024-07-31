27076d005a1f831560a4355635d4e4a4197edc0d
MaksimVasilyev <vasilyev.v.maxim@gmail.com>
2024-07-30 20:50:15 -0600

    Setup project with dependency installation

---
+++ rest-countries/src/app/[countryName]/page.tsx
@@ -0,0 +1,11 @@
+import StyledH1 from "@/components/StyledH1";
+import Title from "@/components/Title";
+
+export default function Home() {
+  return (
+    <>
+      <StyledH1>123</StyledH1>
+      <Title>321</Title>;
+    </>
+  );
+}
